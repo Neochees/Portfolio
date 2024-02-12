2c7d94f42be254f5b85565979e3708e22472ba30
Neochees <113420296+Neochees@users.noreply.github.com>
2024-02-12 13:41:50 +0100

    animation done

--- build/js/script.js
+++ build/js/script.js
@@ -1,29 +1,37 @@
-
-console.log("e")
-
 let animationInProgress = false;
-let animationId
-document.body.addEventListener("click", (e)=>
-{
-    const clickEffect = document.querySelector(".click-effect");
-    if(animationInProgress){
+let animationId;
+const clickEffect = document.querySelector(".click-effect");
+
+document.body.addEventListener("click", (e) => {
+    if (animationInProgress) {
         clearTimeout(animationId);
         clickEffect.classList.remove("effect");
         void clickEffect.offsetWidth;
     }
-    clickEffect.style.top = e.clientY + "px";
+
+    clickEffect.style.top = e.clientY + window.scrollY + "px";
     clickEffect.style.left = e.clientX + "px";
     clickEffect.classList.add("effect");
-    animationInProgress = true
-    animationId = setTimeout(()=>{
+    
+    animationInProgress = true;
+    animationId = setTimeout(() => {
         clickEffect.classList.remove("effect");
         animationInProgress = false;
-    }, 750)
-})
+    }, 750);
+});
 
+// Update click effect position on scroll
+window.addEventListener("scroll", () => {
+    if (animationInProgress) {
+        clearTimeout(animationId);
+        clickEffect.classList.remove("effect");
+        void clickEffect.offsetWidth;
+        animationInProgress = false;
+    }
+});
 
-const ageDivs = document.getElementsByClassName("age")
-age = Date.now() - new Date(2006, 3, 23);
+const ageDivs = document.getElementsByClassName("age");
+let age = Date.now() - new Date(2006, 3, 23);
 age = Math.floor(age / 1000 / 60 / 60 / 24 / 365);
-ageDivs[0].innerText = "I am " + age + " years old"
-ageDivs[1].innerText = "I am " + age + " years old"
+ageDivs[0].innerText = "I am " + age + " years old";
+ageDivs[1].innerText = "I am " + age + " years old";

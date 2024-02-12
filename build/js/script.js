239ec79ff98760af0dc396d3c657acad571c5406
Neochees <113420296+Neochees@users.noreply.github.com>
2024-02-12 13:24:23 +0100

    click animation works

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,27 @@
+
+console.log("e")
+
+let animationInProgress = false;
+let animationId
+document.body.addEventListener("click", (e)=>
+{
+    const clickEffect = document.querySelector(".click-effect");
+    if(animationInProgress){
+        clearTimeout(animationId);
+        clickEffect.classList.remove("effect");
+        void clickEffect.offsetWidth;
+    }
+    clickEffect.style.top = e.clientY + "px";
+    clickEffect.style.left = e.clientX + "px";
+    clickEffect.classList.add("effect");
+    animationInProgress = true
+    animationId = setTimeout(()=>{
+        clickEffect.classList.remove("effect");
+        animationInProgress = false;
+    }, 750)
+})
+
+
 const ageDivs = document.getElementsByClassName("age")
 age = Date.now() - new Date(2006, 3, 23);
 age = Math.floor(age / 1000 / 60 / 60 / 24 / 365);

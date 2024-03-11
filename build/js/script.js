ceaf0970b791065315a245e84342d65f5ad764f0
Neochees <113420296+Neochees@users.noreply.github.com>
2024-03-11 15:39:02 +0100

    hamburgermenu almost done

--- build/js/script.js
+++ build/js/script.js
@@ -2,6 +2,9 @@ let animationInProgress = false;
 let animationId;
 const clickEffect = document.querySelector(".click-effect");
 
+//Animation for click effect
+
+
 document.body.addEventListener("click", (e) => {
     if (animationInProgress) {
         clearTimeout(animationId);
@@ -20,7 +23,6 @@ document.body.addEventListener("click", (e) => {
     }, 750);
 });
 
-// Update click effect position on scroll
 window.addEventListener("scroll", () => {
     if (animationInProgress) {
         clearTimeout(animationId);
@@ -30,8 +32,29 @@ window.addEventListener("scroll", () => {
     }
 });
 
+// dynamic age
+
 const ageDivs = document.getElementsByClassName("age");
 let age = Date.now() - new Date(2006, 3, 23);
 age = Math.floor(age / 1000 / 60 / 60 / 24 / 365);
 ageDivs[0].innerText = "I am " + age + " years old";
 ageDivs[1].innerText = "I am " + age + " years old";
+
+// Hamburger menu code
+
+const initApp = () =>{
+    console.log('werk?')
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const togglemenu = () => {
+        console.log('efee');
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+    }
+
+    hamburgerBtn.addEventListener('click', togglemenu)
+    mobileMenu.addEventListener('click', togglemenu)
+}
+
+document.addEventListener('DOMContentLoader', initApp)
